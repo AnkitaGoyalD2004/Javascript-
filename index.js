e812112592322c8ef82d80818ef3b9da8b77fc50
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-10-21 19:24:43 +0530

    basic concepts

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+var myName = 'Ankita Goyal';
+console.log(myName);
+// there is a type of operator 
+// which tells us a data type of a var
+console.log(typeof(myName));
+
+// Lets checkout some data types example
+console.log(typeof(10 + "20")); // Here we get 1020 because in this concatenation take place
+console.log(typeof("20" - 10));// Here is a bug in this case 
+// this provide me 10 because now type of this is number
+console.log('Ankita' + 'Goyal');
+// In js 1 = true and 0 is false 
+console.log(true + true);// this gives me an output of 2
+console.log(false - true);// this gives me -1
+
+
+//Lets see the use of NaN
+var myPhoneNumber = 'Dishu Goyal';
+console.log(isNaN(myPhoneNumber));
+if(isNaN(myPhoneNumber)){
+    console.log('Please enter a valid phone number');
+}
